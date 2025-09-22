49c4c1e833c7dda259f25cef4d9ce98dead1eca0
Axel Librado Aguilera Alonso <2430331@upv.edu.mx>
2025-09-22 17:36:01 -0600

    Inicie la actividad

---
+++ actividad_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+int main() {
+    std::vector<std::string> nombres;
+    std::vector<std::string> paises;
+    std::vector<std::string> disciplinas;
+    std::vector<char> generos;
+    std::vector<int> medallas;
